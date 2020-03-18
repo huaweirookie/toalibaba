6cdfcec7c867f4bb813b0fbf0f866622887fb075
oscar <youna790049213@gmail.com>
2020-03-18 23:31:34 +0800

    add fortify utils code

--- security/src/main/java/com/zhunongyun/toalibaba/security/controller/FileController.java
+++ security/src/main/java/com/zhunongyun/toalibaba/security/controller/FileController.java
@@ -1,12 +1,15 @@
 package com.zhunongyun.toalibaba.security.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/file")
@@ -14,34 +17,48 @@ public class FileController {
 
     /**
      * 下载任意文件,固定目录+文件名形式
+     *
      * @param response
      * @param fileName
      * @throws IOException
      */
     @GetMapping(value = "/download")
     public void downloadFile(HttpServletResponse response, @RequestParam("fileName") String fileName) throws IOException {
-        // 在/Users/oscar/Downloads/test/download/file/ 目录下存在一个文件 test_file.txt
+        /**
+         * 在/Users/oscar/Downloads/test/download/file/ 目录下存在多个 txt 文件
+         * 正常的业务场景是，前端传入fileName文件名（例如 test.txt），下载/Users/oscar/Downloads/test/download/file/下对应的文件
+         * 黑客在攻击时会可能会传入一个文件路径， 例如 ../../../../etc/passwd等路径，这样就能轻松下载服务器的 passwd 文件
+         */
         String filePath = "/Users/oscar/Downloads/test/download/file/" + fileName;
 
         this.handlerDownloadFile(response, filePath);
     }
 
     /**
-     * 下载任意文件,固定目录+文件名形式
-     * @param response
-     * @param fileName
-     * @throws IOException
+     * 上传任意文件
+     * @param file 上传文件
+     * @return
      */
-    @GetMapping(value = "/upload")
-    public void uploadFile(HttpServletResponse response, @RequestParam("fileName") String fileName) throws IOException {
-        // 在/Users/oscar/Downloads/test/download/file/ 目录下存在一个文件 test_file.txt
-        String filePath = "/Users/oscar/Downloads/test/download/file/" + fileName;
-
-        this.handlerDownloadFile(response, filePath);
+    @PostMapping("/upload")
+    public JSONObject upload(@RequestParam("file") MultipartFile file) {
+        /**
+         * 此处文件上传存在四处安全问题
+         * 1. 未校验文件格式（包含校验文件后缀 和 校验文件内容中的文件格式）
+         * 2. 将上传的文件上传至容器能解析位置
+         * 3. 文件未重命名，重命名后增加黑客攻击难度
+         * 4. 将文件上传地址信息返回前端
+         */
+        try {
+            FileCopyUtils.copy(file.getBytes(), new File("static/" + file.getOriginalFilename()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return JSONObject.parseObject("{\"code\":\"200\", \"msg\":\"文件上传成功\"，\"data\"：{\"filePath\":\"static/xx.xx\"}}");
     }
 
     /**
      * 下载文件公共方法
+     *
      * @param response
      * @param filePath
      */
