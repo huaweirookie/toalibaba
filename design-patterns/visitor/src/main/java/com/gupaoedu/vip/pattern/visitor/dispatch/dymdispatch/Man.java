bfe947e475c94100e833a77d7021c0bce450f517
oscar <youna790049213@gmail.com>
2020-08-13 07:35:05 +0800

    add code

--- design-patterns/visitor/src/main/java/com/gupaoedu/vip/pattern/visitor/dispatch/dymdispatch/Man.java
+++ design-patterns/visitor/src/main/java/com/gupaoedu/vip/pattern/visitor/dispatch/dymdispatch/Man.java
@@ -4,7 +4,6 @@ package com.gupaoedu.vip.pattern.visitor.dispatch.dymdispatch;
  * Created by Tom.
  */
 public class Man implements Person {
-
     public void test() {
         System.out.println("男人");
     }
--- design-patterns/visitor/src/main/java/com/gupaoedu/vip/pattern/visitor/dispatch/dymdispatch/WoMan.java
+++ design-patterns/visitor/src/main/java/com/gupaoedu/vip/pattern/visitor/dispatch/dymdispatch/WoMan.java
@@ -4,7 +4,6 @@ package com.gupaoedu.vip.pattern.visitor.dispatch.dymdispatch;
  * Created by Tom.
  */
 public class WoMan implements Person {
-
     public void test() {
         System.out.println("女人");
     }
