9ba75b53fa64d726286248bf84a821d90c0ac402
oscar <youna790049213@gmail.com>
2020-10-14 17:16:27 +0800

    reset code

--- spring/spring-framework-5.0.2.RELEASE-中文注释版/spring-webmvc/src/main/java/org/springframework/web/servlet/FrameworkServlet.java
+++ spring/spring-framework-5.0.2.RELEASE-中文注释版/spring-webmvc/src/main/java/org/springframework/web/servlet/FrameworkServlet.java
@@ -482,39 +482,6 @@ public abstract class FrameworkServlet extends HttpServletBean implements Applic
 		}
 	}
 
-
-	/**
-	 * Overridden method of {@link HttpServletBean}, invoked after any bean properties
-	 * have been set. Creates this servlet's WebApplicationContext.
-	 */
-	@Override
-	protected final void initServletBean() throws ServletException {
-		getServletContext().log("Initializing Spring FrameworkServlet '" + getServletName() + "'");
-		if (this.logger.isInfoEnabled()) {
-			this.logger.info("FrameworkServlet '" + getServletName() + "': initialization started");
-		}
-		long startTime = System.currentTimeMillis();
-
-		try {
-
-			this.webApplicationContext = initWebApplicationContext();
-			initFrameworkServlet();
-		}
-		catch (ServletException ex) {
-			this.logger.error("Context initialization failed", ex);
-			throw ex;
-		}
-		catch (RuntimeException ex) {
-			this.logger.error("Context initialization failed", ex);
-			throw ex;
-		}
-
-		if (this.logger.isInfoEnabled()) {
-			long elapsedTime = System.currentTimeMillis() - startTime;
-			this.logger.info("FrameworkServlet '" + getServletName() + "': initialization completed in " +
-					elapsedTime + " ms");
-		}
-	}
 	/**
 	 * Overridden method of {@link HttpServletBean}, invoked after any bean properties
 	 * have been set. Creates this servlet's WebApplicationContext.
